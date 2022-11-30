ff1d002ce97ece5e1d087c2eb5cb39fd1b4eae6a
EvgenyQzaza <118588937+EvgenyQzaza@users.noreply.github.com>
2022-11-30 08:16:45 +0300

    Add files via upload

---
+++ Avto/Avto/Program.cs
@@ -0,0 +1,28 @@
+﻿using InspectorLid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Functionlnsp fi = new Functionlnsp();
+            Console.WriteLine(fi.GetInspector());
+            //Console.WriteLine(fi.GetWorker());
+            //Console.WriteLine(fi.GetInspector());
+            //fi.SetInspector("Human H.H");
+            //fi.AddWorker();
+            
+
+
+        }
+
+        
+
+    }
+}
